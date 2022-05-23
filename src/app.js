62597b630993a9f54dca9e72eeb55c65d38df9d2
Pat Shaver <pat.shaver@core10.io>
2022-05-23 10:17:38 -0400

    Commit for Module3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { readFileSync } = require('fs');
 
 const express = require('express');
 
@@ -10,7 +11,34 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+const accountData = readFileSync("./src/json/accounts.json", "utf8");
+const accounts = JSON.parse(accountData);
+
+const userData = readFileSync("./src/json/users.json", "utf8");
+const users = JSON.parse(userData);
+
+app.get('/', (req, res) => res.render('index', { 
+    title: 'Account Summary',
+    accounts: accounts 
+    }
+));
+
+app.get('/savings', (req, res) => res.render('account', {
+    account: accounts.savings
+}
+));
+
+app.get('/checking', (req, res) => res.render('account', {
+    account: accounts.checking
+}
+));
+
+app.get('/credit', (req, res) => res.render('account', {
+    account: accounts.credit
+}
+));
+
+app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!');
