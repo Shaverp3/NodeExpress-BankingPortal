1644ce499ba02983a7eb9550f014eecd69ec13f1
Pat Shaver <pat.shaver@core10.io>
2022-05-23 10:34:03 -0400

    fixed Module3 issues

--- src/app.js
+++ src/app.js
@@ -17,30 +17,28 @@ const accounts = JSON.parse(accountData);
 const userData = readFileSync("./src/json/users.json", "utf8");
 const users = JSON.parse(userData);
 
-app.get('/', (req, res) => res.render('index', { 
-    title: 'Account Summary',
-    accounts: accounts 
-    }
-));
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Account Summary',
+    accounts: accounts});
+});
 
-app.get('/savings', (req, res) => res.render('account', {
-    account: accounts.savings
-}
-));
+app.get('/savings', (req, res) => {
+    res.render('account', { account: accounts.savings });
+});
 
-app.get('/checking', (req, res) => res.render('account', {
-    account: accounts.checking
-}
-));
+app.get('/checking', (req, res) => {
+    res.render('account', { account: accounts.checking });
+});
 
-app.get('/credit', (req, res) => res.render('account', {
-    account: accounts.credit
-}
-));
+app.get('/credit', (req, res) => {
+    res.render('account', { account: accounts.credit });
+});
 
-app.get('/profile', (req, res) => res.render('profile', { user: users[0] }));
+app.get('/profile', (req, res) => {
+    res.render('profile', { user: users[0] });
+});
 
 app.listen(3000, () => {
     console.log('PS Project Running on port 3000!');
-  });
+});
   
